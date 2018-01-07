e52472f3fae5abebcfb8734c6db19df76aaaa7be
Kosuke Ohmura <bnpb81@gmail.com>
2018-01-08 01:33:31 +0900

    fix build error

--- GrpcSample/GrpcSample/ViewController.swift
+++ GrpcSample/GrpcSample/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import gRPC
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func didTap(_ sender: UIButton) {
+        let service = Echo_EchoService(address: "192.168.1.191:50051", secure: false)
 
+        let request = Echo_EchoRequest.with { $0.text = "this is the first request" }
+        do {
+            let response = try service.get(request)
+            print("sync: \(response.text)")
+        } catch {
+            print("error:", error)
+        }
+    }
 
 }
-
--- GrpcSample/Protoc/run_protoc.sh
+++ GrpcSample/Protoc/run_protoc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 PROTOC_DIR=$(cd $(dirname $0); pwd)
 docker run --rm -it \
-  -v $PROTOC_DIR/Generated:$PROTOC_DIR/Generated \
+  -v $PROTOC_DIR/generated:$PROTOC_DIR/generated \
   -v $PROTOC_DIR/proto:$PROTOC_DIR/proto \
   -w $PROTOC_DIR \
   grpc/swift \
   protoc \
-    --swift_out=$PROTOC_DIR/Generated/ \
-    --swiftgrpc_out=$PROTOC_DIR/Generated/ \
+    --swift_out=$PROTOC_DIR/generated/ \
+    --swiftgrpc_out=$PROTOC_DIR/generated/ \
     --proto_path=$PROTOC_DIR \
     $PROTOC_DIR/proto/*.proto
